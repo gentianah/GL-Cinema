70ddb9df50ef5ae0c8f554417312283ef1859bd4
gentianah <121191968+gentianah@users.noreply.github.com>
2023-01-31 20:21:40 +0100

    Adding ViewModel

---
+++ GL-Cinema/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,14 @@
+﻿using LGCinema.Data.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LGCinema.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
